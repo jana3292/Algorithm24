9b38e28740446ae70b9ea231bd35acbde0f324d7
jana3292 <jana765321@gmail.com>
2024-04-02 23:39:32 +0900

    하이

--- chap02/순환적인 팩토리얼 알고리즘 2-7.py
+++ chap02/순환적인 팩토리얼 알고리즘 2-7.py
@@ -1,5 +1,10 @@
-def factorial(n):
-    if n == 1:
-        return 1
+def hanoi_tower(n, fr, tmp, to):
+    if (n == 1):
+        print("원판 1: %s --> %s" % (fr, to))
     else:
-        return n * factorial(n-1) 
+        hanoi_tower(n - 1, fr, to, tmp)
+        print("원판 %d: %s --> %s" % (n,fr,to))
+        hanoi_tower(n - 1, tmp, fr, to)
+
+        n = int(input("원판의 갯수는 >>>"))
+        hanoi_tower(n, 'A', 'B', 'C')
