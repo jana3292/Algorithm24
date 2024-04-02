5f5f976cb03fab0077ae356fcb81ff74cf8d0662
jana3292 <jana765321@gmail.com>
2024-04-02 23:53:06 +0900

    gd

---
+++ chap02/자연수의 제곱 계산(O(n)) 2-3.py
@@ -0,0 +1,9 @@
+def compute_square_B(n) :
+    sum = 0
+    for i in range(n):
+        sum = sum + n;
+    return sum
+
+n = int(input("제곱하고 싶은 숫자를 입력하시오 : "))
+
+print(n, "의 제곱은 ", compute_square_B(n), "입니다.")
